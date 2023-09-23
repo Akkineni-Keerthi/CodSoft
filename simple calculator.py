e606ff136048b7c6114de9626b6d004335634f2b
Akkineni-Keerthi <145860653+Akkineni-Keerthi@users.noreply.github.com>
2023-09-23 22:00:24 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,40 @@
+def add(a, k):
+    return a + k
+
+def subtract(a, k):
+    return a - k
+
+def multiply(a, k):
+    return a * k
+
+def divide(a, k):
+    if k != 0:
+        return a / k
+    else:
+        return "Error: Division by zero"
+
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+    else:
+        print("Invalid input")
+
+
+calculator()
